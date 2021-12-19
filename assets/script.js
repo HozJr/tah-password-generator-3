76cefa38c21cbd15790b886242ec15dc78bfdddd
HozJr <hozjr2006@gmail.com>
2021-12-18 22:54:55 -0600

    Everything works except Capital Letters

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var confirmNumber;
 var confirmLowercase;
 var confirmUppercase;
 var choices;
+var pass;
 
 
 // Variable values for each confirm
@@ -19,13 +20,13 @@ letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
 letter2 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Special characters
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 
 var generateBtn = document.querySelector("#generate");
 
 generateBtn.addEventListener("click", function () {
-   var pass = generatePassword();
+   pass = generatePassword();
    document.getElementById("password").placeholder = pass;
 });
 
@@ -34,21 +35,21 @@ function generatePassword() {
     enter = parseInt(prompt("How many characters would you like your password to be? Choose between 8 and 128"));
     if (!enter) {
         alert("This needs a value");
-    } else if (enter <8 || enter > 128) {
-        enter - parseInt(prompt("You must choose between 8 and 128"));
+    } else if (enter < 8 || enter > 128) {
+        enter = parseInt(prompt("You must choose between 8 and 128"));
 
     } else {
         // Other user inputs
         confirmNumber = confirm("Will password contain numbers?");
         confirmCharacter = confirm("Will password contain special characters?");
         confirmLowercase = confirm("Will password contain Lowercase letters");
-        confrimUppercase = confirm("Will password contain Uppercase letters?");
+        confirmUppercase = confirm("Will password contain Uppercase letters?");
     };
 
     // If 4 negative responses
 
     if (!confirmNumber && !confirmCharacter && !confirmLowercase && !confirmUppercase) {
-        choices = alert("You miust choose a criteria!");
+        choices = alert("You must choose a criteria!");
     }
 
     // If 4 positive responses
@@ -60,7 +61,7 @@ function generatePassword() {
     else if (confirmNumber && confirmCharacter && confirmLowercase) {
         choices = number.concat(character, letter);
     }
-    else if (confirmNumber && confirmCharacter && corfirmUppercase) {
+    else if (confirmNumber && confirmCharacter && confirmUppercase) {
         choices = number.concat(character, letter2);
     }
     else if (confirmNumber && confirmLowercase && confirmUppercase) {
@@ -107,7 +108,7 @@ function generatePassword() {
 var password = [];
 
 // Random selection for all variables
-for (var i = 0, i < enter; i++) {
+for (var i = 0; i < enter; i++) {
     var pickChoices = choices[Math.floor(Math.random() * choices.length)];
     password.push(pickChoices);
 }
@@ -122,23 +123,3 @@ return pass;
 function UserInput(pass) {
     document.getElementById("password").textContent = pass;
 }
-
-
-
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
